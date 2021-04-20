e564af314d0b6f9eed090ed06ac3d3e17f44b560
Karolina <kmieczkowska75@gmail.com>
2021-04-20 21:07:35 +0100

    Add carousel, diverse fonts, finish styling

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+// Mobile nav
+
 document.querySelector(".burger-icon").addEventListener("click", function() {
     document.querySelector(".mobile-nav").classList.toggle("mobile-nav-active");
+})
+
+// Carousel
+
+const slider = document.querySelector('.slider');
+const leftArrow = document.querySelector('.left');
+const rightArrow = document.querySelector('.right');
+const controlDots = document.querySelectorAll('.controls li');
+
+let sectionIndex = 0;
+
+const setIndex = () => {
+    document.querySelector('.controls .selected').classList.remove('selected');
+    slider.style.transform = 'translate('+ (sectionIndex) * - 33.33 +'%)';
+}
+
+rightArrow.addEventListener('click', () => {
+    sectionIndex = (sectionIndex < 2) ? sectionIndex + 1 : 2;
+    setIndex();
+    controlDots[sectionIndex].classList.add('selected');
+})
+
+leftArrow.addEventListener('click', () => {
+    sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0;
+    setIndex();
+    controlDots[sectionIndex].classList.add('selected') 
+})
+
+controlDots.forEach((indicator, ind) => {
+    indicator.addEventListener('click', () => {
+        sectionIndex = ind;
+        setIndex();
+        indicator.classList.add('selected');
+    })
 })
